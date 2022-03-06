97f1aa9d8447f8c244a03e07b9c847b6c565f664
Hakan GÖLGE <58719777+hakanglg@users.noreply.github.com>
2022-03-07 00:15:51 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:otodo/features/model/task_model.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/dark_theme.dart';
 import 'features/model/tasks_model.dart';
 import 'features/tasks/view/tasks_view.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Tasks().createPrefObject();
-
-
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  await Hive.openBox("taskBox");
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
       create: (context) => Tasks(),
